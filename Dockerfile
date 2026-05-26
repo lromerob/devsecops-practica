a60d38abe0ba58549d9c9f261031c76238f27190
Leonardo Romero <laromero@elior.net>
2026-05-26 18:49:44 +0200

    feat: reestructuracion completa y limpia del entorno devsecops

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.11-alpine
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+RUN addgroup -S appgroup && adduser -S appuser -G appgroup
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir --break-system-packages -r requirements.txt
+
+COPY app.py .
+
+RUN chown -R appuser:appgroup /app
+
+USER appuser
+
+EXPOSE 5000
+
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, jsonify
 import os
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 app = Flask(__name__)
 
+VAULT_NAME = "kv-devsecops"
+KEY_VAULT_URL = f"https://{VAULT_NAME}.vault.azure.net/"
+
+def obtener_secreto_seguro():
+    try:
+        credential = DefaultAzureCredential()
+        client = SecretClient(vault_url=KEY_VAULT_URL, credential=credential)
+        secret = client.get_secret("DB-CONNECTION")
+        return secret.value
+    except Exception as e:
+        return "Simulado: Server=localhost;User=appuser;Password=LocalSecurePass123!"
+
 @app.route('/')
 def home():
+    secreto_recuperado = obtener_secreto_seguro()
     return jsonify({
         "status": "running",
-        "context": "DevSecOps Pr?ctica 4",
-        "user_mode": "non-root"
+        "context": "DevSecOps Pr?ctica 4 - Final",
+        "user_mode": "non-root",
+        "key_vault_target": VAULT_NAME,
+        "database_secret": secreto_recuperado
     })
 
 if __name__ == '__main__':
